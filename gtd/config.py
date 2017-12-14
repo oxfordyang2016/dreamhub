c69346f11dd4960c5b1c5d9ffb41917aa4b823ec
RCY-FUDAN <rcy-fudan@ioeb-FUDANdeMacBook-Pro.local>
2017-12-14 13:45:36 +0800

    add update funtion

--- gtd/config.py
+++ gtd/config.py
@@ -1,5 +1,5 @@
-url = "https://gtdofdream.herokuapp.com"
-
+#url = "https://gtdofdream.herokuapp.com"
+url = "http://47.100.100.141:5000"
 
 
 
--- gtd/gtd.py
+++ gtd/gtd.py
@@ -8,9 +8,29 @@ import requests
 #@click.option('--name', prompt='Your name',help='The person to greet.')
 #@click.option('--t', default="i am test")
 @click.option('--inbox',help="u should write anything in ur mind")
-@click.option('--tasktime',default="have not been deal with")
 @click.option('--taskstatus',default="this status is not finised")
-def task(inbox,tasktime,taskstatus):
+@click.option('--today',default="noinfo")
+@click.option('--tasktime',default="have not been deal with")
+@click.option('--update',default="noupdate")
+@click.option('--id',default="noselect")
+@click.option('--project',default="inbox")
+@click.option('--plantime',default="--not--clarified",help="it is used to specify the task plan time")
+def task(inbox,tasktime,plantime,taskstatus,today,update,id,project):
+    if str(today)!="noinfo":
+        r= requests.get(str(url)+'/today')
+        #print(r.text)
+        for k,v in r.json().iteritems():
+            print "id is "+str(k)+" task is "+str(v)
+
+        return "allok"
+    if str(update)!="noupdate":
+        id = str(id)
+        project = str(project)
+        taskstatus=str(taskstatus)
+        r=requests.post(str(url)+'/update',data={"id":id,"project":project,"plantime":plantime,"taskstatus":taskstatus})   
+        print(r.text)
+        return "allok"
+
     inboxthing = str(inbox)
     inputtime = str((time.ctime()))
     specifytasktime = str(tasktime) 
